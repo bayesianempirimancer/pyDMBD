46f29bd4301dc8ef2e66ef87e42aa41a2c604da9
Jeff Beck <brains4lunch@gmail.com>
2023-07-04 15:55:54 -0400

    Delete PoissonMixtureModel.py

--- PoissonMixtureModel.py
+++
@@ -1,7 +0,0 @@
-import torch
-from dists import Mixture, Gamma
-class PoissonMixtureModel(Mixture):
-    def __init__(self,alpha_0,beta_0):
-        dist = Gamma(alpha_0,beta_0).to_event(1)
-        super().__init__(dist)
-
