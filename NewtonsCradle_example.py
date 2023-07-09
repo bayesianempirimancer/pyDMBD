f6b1a862966612524f7f9c8f077a9afa2dfdf0ac
Jeff Beck <brains4lunch@gmail.com>
2023-07-08 21:12:08 -0400

    Add files via upload

--- NewtonsCradle_example.py
+++ NewtonsCradle_example.py
@@ -5,7 +5,7 @@ from matplotlib.animation import FuncAnimation, FFMpegWriter
 from matplotlib import cm 
 from simulations.NewtonsCradle import NewtonsCradle
 
-dmodel = NewtonsCradle(n_balls=5,ball_size=0.2,Tmax=500,batch_size=40,g=1,leak=0.05/8,dt=0.05,include_string=False) 
+dmodel = NewtonsCradle(n_balls=5,ball_size=0.2,Tmax=500,batch_size=20,g=1,leak=0.05/8,dt=0.05,include_string=False) 
 
 data_temp = dmodel.generate_data('random')[0]
 data_0 = data_temp[0::5]
@@ -13,32 +13,32 @@ data_temp = dmodel.generate_data('1 ball object')[0]
 data_1 = data_temp[0::5]
 data_temp = dmodel.generate_data('2 ball object')[0]
 data_2 = data_temp[0::5]
-data_temp = dmodel.generate_data('3 ball object')[0]
-data_3 = data_temp[0::5]
-data_temp = dmodel.generate_data('4 ball object')[0]
-data_4 = data_temp[0::5]
+# data_temp = dmodel.generate_data('3 ball object')[0]
+# data_3 = data_temp[0::5]
+# data_temp = dmodel.generate_data('4 ball object')[0]
+# data_4 = data_temp[0::5]
 data_temp = dmodel.generate_data('1 + 1 ball object')[0]
 data_11 = data_temp[0::5]
 data_temp = dmodel.generate_data('2 + 2 ball object')[0]
 data_22 = data_temp[0::5]
 data_temp = dmodel.generate_data('1 + 2 ball object')[0]
 data_12 = data_temp[0::5]
-data_temp = dmodel.generate_data('2 + 3 ball object')[0]
-data_23 = data_temp[0::5]
-data_temp = dmodel.generate_data('1 + 3 ball object')[0]
-data_13 = data_temp[0::5]
+# data_temp = dmodel.generate_data('2 + 3 ball object')[0]
+# data_23 = data_temp[0::5]
+# data_temp = dmodel.generate_data('1 + 3 ball object')[0]
+# data_13 = data_temp[0::5]
 
-datas = (data_0,data_1,data_2,data_3,data_4,data_11,data_12,data_13,data_22,data_23)
+datas = (data_0,data_1,data_2,data_11,data_12,data_22)#,data_3,data_4,data_11,data_12,data_13,data_22,data_23)
 dy = torch.zeros(2)
 dy[1] = 1.0
 new_datas = ()
 for k,data in enumerate(datas):
-    data_temp = data + dy
-    v_data = torch.diff(data_temp,dim=0)
+    data = data + dy
+    v_data = torch.diff(data,dim=0)
     v_data = v_data/v_data.std()
-    new_datas = new_datas + (torch.cat((data_temp[1:],v_data),dim=-1),)
-
+    new_datas = new_datas + (torch.cat((data[1:],v_data),dim=-1),)
 datas = new_datas
+
 
 # num_mixtures = 5
 # batch_shape = ()
@@ -67,15 +67,16 @@ datas = new_datas
 #     model.update(data,None,None,iters=20,latent_iters=1,lr=0.5)
 #     ELBO.append(model.ELBO())
 
-data = torch.cat(datas[0:3],dim=1)
+data = torch.cat(datas[0:6],dim=1)
+print('simulations complete')
 
-model = DMBD(obs_shape=data.shape[-2:],role_dims=(8,4,8),hidden_dims=(4,2,4),batch_shape=(),regression_dim = 0, control_dim=0)
+model = DMBD(obs_shape=data.shape[-2:],role_dims=(4,4,4),hidden_dims=(4,4,4),batch_shape=(),regression_dim = -1, control_dim=0)
 # model0 = DMBD(obs_shape=data.shape[-2:],role_dims=(20,0,0),hidden_dims=(10,0,0),batch_shape=(),regression_dim = -1, control_dim=0)
 # model0.update(data,None,None,iters=2*iters,latent_iters=1,lr=0.5,verbose=True)
 # f = r"./cradle0.mp4"
 # ar = animate_results('role',f, xlim = (-1.5,1.5), ylim = (-0.2,1.2), fps=10)
 # ar.make_movie(model0, data, (0,40,60,80))#,120))#,60,61,80,81))
-iters = 20
+iters = 40
 r1 = model.role_dims[0]
 r2 = r1+model.role_dims[1]
 r3 = r2+ model.role_dims[2]
@@ -84,9 +85,8 @@ h2 = h1+model.hidden_dims[1]
 h3 = h2+ model.hidden_dims[2]
 batch_num = 50
 
-
 for i in range(iters):
-    model.update(data,None,None,iters=2,latent_iters=1,lr=0.5+0.5*(i>iters/2),verbose=True)
+    model.update(data,None,None,iters=1,latent_iters=1,lr=0.5,verbose=True)
 
 #    batch_num = torch.randint(0,data.shape[1],(1,)).item()
     sbz=model.px.mean()
@@ -154,9 +154,9 @@ zz = zz/zz.std()
 
 fig, axs = plt.subplots(2, 1, sharex=True)
 
-axs[0].plot(ss[:,batch_num,-1:],'b',label='s')
+axs[0].plot(ss[:,batch_num,-1:],'r',label='s')
 axs[0].plot(bb[:,batch_num,-1:],'g',label='b')
-axs[0].plot(zz[:,batch_num,-1:],'r',label='z')
+axs[0].plot(zz[:,batch_num,-1:],'b',label='z')
 axs[0].set_title('Top PC Score')
 # handles, labels = axs[0].get_legend_handles_labels()
 # selected_handles = [handles[0], handles[2], handles[4]]
@@ -164,9 +164,9 @@ axs[0].set_title('Top PC Score')
 # axs[0].legend(selected_handles, selected_labels)
 axs[0].legend()
 
-axs[1].plot(p[:,batch_num,0],'b')
+axs[1].plot(p[:,batch_num,0],'r')
 axs[1].plot(p[:,batch_num,1],'g')
-axs[1].plot(p[:,batch_num,2],'r')
+axs[1].plot(p[:,batch_num,2],'b')
 axs[1].set_title('Number of Assigned Objects')
 axs[1].set_xlabel('Time')
 #plt.savefig('C://Users/brain/Desktop/cradlePCs1.png')
@@ -177,7 +177,7 @@ print('Generating Movie...')
 # f = r"C://Users/brain/Desktop/cradle.mp4"
 f = r"./cradle.mp4"
 ar = animate_results('sbz',f, xlim = (-1.5,1.5), ylim = (-0.2,1.2), fps=10)
-ar.make_movie(model, data, (0,40,60,80))#,120))#,60,61,80,81))
+ar.make_movie(model, data, (0,20,40,60,80,100))#,120))#,60,61,80,81))
 
 batch_num = 40
-plt.scatter(data[:,batch_num,:,0],data[:,batch_num,:,1],cmap='rainbow',c=model.obs_model.p.argmax(-1)[:,batch_num,:])
+plt.scatter(data[:,batch_num,:,0],data[:,batch_num,:,1],cmap='rainbow_r',c=model.obs_model.p.argmax(-1)[:,batch_num,:])
